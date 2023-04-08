0b433b32032b9e8621f672eb041c6ace3fb92530
Mykola <odesalover@gmail.com>
2023-04-08 12:44:15 +0300

    hw-54

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent {
+  title = 'High-Performance Computing';
+ text = 'High-performance Computing (HPC) is the ability to process data and perform complex calculation at high speeds. HPC is one of the most essential tools fueling the advancement of computational science..';
+}
